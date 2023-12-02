4b377f681fbcad9e0b83246a29a9d5d6e5d9bbc7
kanchan <kanchankotkar001@gmail.com>
2023-12-02 22:34:25 +0530

    second commit

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
+import I18n from "./../../utils/I18n";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+
+    // const [lang, setLang] = useState("en");
+    return (
+        <div className='outerbox'>
+            <h1>{I18n({ keyword: "WelcomeMessage" })}</h1>
+
+            <p> {I18n({ keyword: "normalMessage" })}</p>
+
+            <h3> {I18n({ keyword: "greetingMessage" })}</h3>
+
+
+
+
+            <select className='btn'
+                defaultValue={localStorage.getItem("lang")}
+                onChange={(e) => {
+                    localStorage.setItem("lang", e.target.value);
+                    window.location.reload();
+                }}>
+                <option value="mr">Marathi</option>
+                <option value="en">English</option>
+
+                <option value="hi">Hindi</option>
+
+            </select>
+
+        </div>
+    )
 }
 
 export default Home
